d66d5183d6aa7d35356828199a0f96a36ad2f377
Max Gurtovoy <maxg@mellanox.com>
2017-06-21 17:14:51 +0300

    nvmet: add peer_to_peer_sqe_inline_size query for transport ops
    In P2P case, each transport type will set sqe_inline_size according
    to its HCA capabilities.
    
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- drivers/nvme/target/nvmet.h
+++ drivers/nvme/target/nvmet.h
@@ -218,6 +218,7 @@ struct nvmet_fabrics_ops {
 	int (*install_offload_queue)(struct nvmet_ctrl *ctrl, u16 qid);
 	int (*create_offload_ctrl)(struct nvmet_ctrl *ctrl);
 	void (*destroy_offload_ctrl)(struct nvmet_ctrl *ctrl);
+	unsigned int (*peer_to_peer_sqe_inline_size)(struct nvmet_ctrl *ctrl);
 };
 
 #define NVMET_MAX_INLINE_BIOVEC	8
